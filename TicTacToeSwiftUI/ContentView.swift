9c752c3b6866c88e78c7d1896ce746e8c45906ec
Pedro Henrique Ramos <pedrohscramos@gmail.com>
2023-11-06 14:56:45 -0300

    Initial Commit

---
+++ TicTacToeSwiftUI/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Pedro Henrique Souza Cruz Ramos on 06/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @StateObject var gameState = GameState()
+    
+    var body: some View
+    {
+        let borderSize = CGFloat(5)
+        
+        VStack(spacing: borderSize)
+        {
+            ForEach(0...2, id: \.self)
+            {
+                row in
+                HStack(spacing: borderSize)
+                {
+                    ForEach(0...2, id: \.self)
+                    {
+                        column in
+                        
+                        let cell = gameState.board[row][column]
+                        
+                        Text(cell.displayTile())
+                            .font(.system(size:60))
+                            .foregroundColor(cell.tileColor())
+                            .bold()
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+                            .onTapGesture {
+                                gameState.placeTile(row, column)
+                            }
+                    }
+                }
+            }
+        }
+        .background(Color.black)
+        .padding()
+        .alert(isPresented: $gameState.showAlert){
+            Alert(title: Text(gameState.alertMessage),
+                  dismissButton: .default(Text("Okay")){
+                gameState.resetBoard()
+            }
+            )
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by Pedro Henrique Souza Cruz Ramos on 06/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
